02dc98c7d5ca3226de19e2505d63dfd5c82957cb
caisw <1374632238@qq.com>
2023-01-02 22:13:01 +0800

    feat：电影背景色

--- src/main/java/com/example/demo/movie/model/MovieSysDict.java
+++ src/main/java/com/example/demo/movie/model/MovieSysDict.java
@@ -1,15 +1,15 @@
 package com.example.demo.movie.model;
 
-import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
-import java.util.Date;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * <p>
@@ -41,5 +41,6 @@ public class MovieSysDict implements Serializable {
 
     private String value;
 
+    private String color;
 
 }
